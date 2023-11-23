3fed48c10eb6e581e4ecbc3c8382079c10c1160e
Raian M. Gil del Rosario <rgil@lanzasoftware.com>
2023-11-22 22:35:08 -0400

    init app

--- src/environments.ts
+++ src/environments.ts
@@ -1,2 +1,2 @@
 export const mongoUri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/Porfolio?authSource=admin"
-export const port = process.env.PORT || 3000
+export const port = process.env.PORT || 3001
